d0ab42f1bc4c66aeb8c5c5754261ea4d24dcaba8
NOWSHIN AKTER <akter.nowshin@head-blocks.com>
2025-07-08 13:21:08 +0600

    become seller note added

--- src/pages/Home/Home/BecomeSeller/BecomeSeller.jsx
+++ src/pages/Home/Home/BecomeSeller/BecomeSeller.jsx
@@ -93,6 +93,21 @@ const BecomeSeller = () => {
 
             {/* Modal Body */}
             <div className="p-6">
+              {/* Important Note */}
+              <div className="mb-6 p-4 bg-blue-50 border border-blue-200 rounded-lg">
+                <div className="flex items-start">
+                  <svg className="w-5 h-5 text-blue-600 mt-0.5 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                    <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+                  </svg>
+                  <div>
+                    <h4 className="text-blue-800 font-semibold mb-1">Important Notice</h4>
+                    <p className="text-blue-700 text-sm">
+                      Please register first then send the request to become a "seller". Make sure you have a valid account before submitting this form.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
               <form onSubmit={handleSubmit}>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   {/* Name */}
